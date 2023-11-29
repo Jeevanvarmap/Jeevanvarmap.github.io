b28df4307d2a6b77a0a0f58e3980a8a37a84207b
Jeevan Varma <jeevanvarma.p@gmail.com>
2023-11-29 08:46:29 +0530

    Create final_year_project.php

---
+++ final_year_project.php
@@ -0,0 +1,6 @@
+<html>
+<?php  
+header("Location: https://jeevanvarmap.notion.site/Title-Design-Development-of-swarm-AGV-s-alliance-for-Search-Rescue-Operations-df63a67ce1874f8eafcb6d8a0d84a3eb?pvs=4", true, 301);  
+exit();  
+?>
+</html>
